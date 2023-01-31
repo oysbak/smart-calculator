a408062b17af8e8c110e9c93ae797df46e2aa768
oysteinbakken <oystein.bakken@nav.no>
2023-01-31 21:41:24 +0100

    Stage 2/8: 2+2+

--- Main.kt
+++ Main.kt
@@ -1,6 +1,26 @@
 package calculator
 
 fun main() {
-    val numbers = readln().split(" ").map { it.toInt() }
-    println(numbers[0] + numbers[1])
+    do {
+        when (val userInput = readln()) {
+            "" -> {
+                // do nothing
+            }
+            
+            "/exit" -> {
+                println("Bye!")
+                return
+            }
+            
+            else -> {
+                println(Calculator.sum(userInput.split(" ").map { it.toInt() }))
+            }
+        }
+    } while (true)
+}
+
+class Calculator {
+    companion object {
+        fun sum(numbers: List<Int>): Int = numbers.sum()
+    }
 }
