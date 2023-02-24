072471e342e4ace7e5683330afa3eb8754acfb24
oysteinbakken <oystein.bakken@nav.no>
2023-02-24 09:54:43 +0100

    Stage 4/8: Add subtractions

--- Main.kt
+++ Main.kt
@@ -2,7 +2,7 @@ package calculator
 
 fun main() {
     do {
-        when (val userInput = readln()) {
+        when (val inputString = readln()) {
             "" -> {
                 // do nothing
             }
@@ -17,14 +17,45 @@ fun main() {
             }
             
             else -> {
-                println(Calculator.sum(userInput.split(" ").map { it.toInt() }))
+                UserInput(inputString)
             }
         }
     } while (true)
 }
 
-class Calculator {
-    companion object {
-        fun sum(numbers: List<Int>): Int = numbers.sum()
+class UserInput(input: String) {
+    private val array = input.split(" ")
+    
+    init {
+        var product = 0
+        var operation = '+'
+        array.forEach {
+            if (it.toIntOrNull() == null) {
+                operation = extractSinglePrefiks(it)
+            } else {
+                product = calculate(product, it.toInt(), operation)
+            }
+        }
+        println(product)
+    }
+    
+    private fun calculate(x: Int, y: Int, operation: Char): Int {
+        if (operation == '+')
+            return x + y
+        if (operation == '-')
+            return x - y
+        return 0
+    }
+    
+    private fun extractSinglePrefiks(input: String): Char {
+        var value = '+'
+        input.forEach {
+            value = prefix(value, it)
+        }
+        return value
+    }
+    
+    private fun prefix(first: Char, second: Char): Char {
+        return if (first == second) '+' else '-'
     }
 }
